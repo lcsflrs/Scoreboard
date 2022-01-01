fb630ef1a01e2a00a445b2cf589c026477337789
Lucas Flores <lcsflrs@gmail.com>
2022-01-01 12:43:46 -0300

    App Implementation

--- App.tsx
+++ App.tsx
@@ -11,9 +11,12 @@
 
 import React, {useEffect} from 'react';
 import {
+  NativeSyntheticEvent,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TextInputEndEditingEventData,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -24,6 +27,65 @@ const App = () => {
   const [maxScore, setMaxScore] = React.useState(initialMaxScore);
   const [countLeft, setCountLeft] = React.useState(0);
   const [countRight, setCountRight] = React.useState(0);
+  const [nameLeft, setNameLeft] = React.useState('TM1');
+  const [nameRight, setNameRight] = React.useState('TM2');
+  const [lastPoint, setLastPoint] = React.useState('');
+
+  const [leftNamePressed, setLeftNamePressed] = React.useState(false);
+  const [rightNamePressed, setRightNamePressed] = React.useState(false);
+  const [maxScorePressed, setMaxScorePressed] = React.useState(false);
+
+  const handleMaxScorePress = () => {
+    if (!isLeftWinner() && !isRightWinner()) {
+      console.log('maxScorePressed');
+      setMaxScorePressed(true);
+    }
+  };
+
+  const handleMaxScoreSubmit = (
+    e: NativeSyntheticEvent<TextInputEndEditingEventData>,
+  ) => {
+    const newMaxScore = parseInt(e.nativeEvent.text, 10);
+    if (newMaxScore > 0) {
+      setMaxScore(newMaxScore);
+      setInitialMaxScore(newMaxScore);
+    }
+    setMaxScorePressed(false);
+  };
+
+  const handleLeftNamePress = () => {
+    if (!isRightWinner() && !isLeftWinner()) {
+      setLeftNamePressed(true);
+    }
+  };
+
+  const handleLeftNameSubmit = (
+    e: NativeSyntheticEvent<TextInputEndEditingEventData>,
+  ) => {
+    if (e.nativeEvent.text) {
+      if (e.nativeEvent.text !== 'KKK') {
+        setNameLeft(e.nativeEvent.text);
+      }
+    }
+    setLeftNamePressed(false);
+  };
+
+  const handleRightNamePress = () => {
+    if (!isRightWinner() && !isRightWinner()) {
+      setRightNamePressed(true);
+    }
+  };
+
+  const handleRightNameSubmit = (
+    e: NativeSyntheticEvent<TextInputEndEditingEventData>,
+  ) => {
+    if (e.nativeEvent.text) {
+      if (e.nativeEvent.text !== 'KKK') {
+        setNameRight(e.nativeEvent.text);
+      }
+    }
+    setRightNamePressed(false);
+  };
 
   const winner = () => {
     if (countLeft === maxScore) {
@@ -34,8 +96,6 @@ const App = () => {
     }
     return '';
   };
-
-  const [lastPoint, setLastPoint] = React.useState('');
 
   const isLeftWinner = () => {
     return winner() === 'Left';
@@ -49,6 +109,18 @@ const App = () => {
     if (countLeft < maxScore) {
       setCountLeft(countLeft + 1);
       setLastPoint('Left');
+    }
+  };
+
+  const decrementLeft = () => {
+    if (countLeft > 0) {
+      setCountLeft(countLeft - 1);
+    }
+  };
+
+  const decrementRight = () => {
+    if (countRight > 0) {
+      setCountRight(countRight - 1);
     }
   };
 
@@ -69,6 +141,7 @@ const App = () => {
     setCountLeft(0);
     setCountRight(0);
     setMaxScore(initialMaxScore);
+    setLastPoint('');
   };
 
   const textLeftStyle = () => {
@@ -115,7 +188,23 @@ const App = () => {
 
           elevation: 16,
         }}>
-        <Text style={{fontSize: 36, color: '#00000098'}}>{maxScore}</Text>
+        {!maxScorePressed ? (
+          <Text
+            onPress={handleMaxScorePress}
+            style={{fontSize: 36, color: '#00000098'}}>
+            {maxScore}
+          </Text>
+        ) : (
+          <TextInput
+            onEndEditing={handleMaxScoreSubmit}
+            placeholder={maxScore.toString()}
+            placeholderTextColor="#00000098"
+            maxLength={2}
+            disableFullscreenUI
+            keyboardType="number-pad"
+            style={{fontSize: 36, color: '#00000098', paddingVertical: 0}}
+          />
+        )}
       </View>
       <View
         style={[
@@ -130,9 +219,27 @@ const App = () => {
             color="white"
           />
         )}
-        <Text style={[styles.teamName, textLeftStyle()]}>Nome</Text>
+        {!leftNamePressed ? (
+          <Text
+            onPress={handleLeftNamePress}
+            style={[styles.teamName, textLeftStyle()]}>
+            {nameLeft}
+          </Text>
+        ) : (
+          <TextInput
+            autoFocus
+            autoCapitalize="characters"
+            style={styles.inputName}
+            onEndEditing={handleLeftNameSubmit}
+            placeholder={nameLeft}
+            placeholderTextColor={'#FFFFFF98'}
+            maxLength={3}
+            disableFullscreenUI
+          />
+        )}
         <TouchableOpacity
           onPress={incrementLeft}
+          onLongPress={decrementLeft}
           disabled={isLeftWinner() || isRightWinner()}>
           <Text style={[styles.score, textLeftStyle()]}>{countLeft}</Text>
         </TouchableOpacity>
@@ -162,9 +269,27 @@ const App = () => {
             color="white"
           />
         )}
-        <Text style={[styles.teamName, textRightStyle()]}>Nome</Text>
+        {!rightNamePressed ? (
+          <Text
+            onPress={handleRightNamePress}
+            style={[styles.teamName, textRightStyle()]}>
+            {nameRight}
+          </Text>
+        ) : (
+          <TextInput
+            autoFocus
+            autoCapitalize="characters"
+            style={styles.inputName}
+            onEndEditing={handleRightNameSubmit}
+            placeholder={nameRight}
+            placeholderTextColor={'#FFFFFF98'}
+            maxLength={3}
+            disableFullscreenUI
+          />
+        )}
         <TouchableOpacity
           onPress={incrementRight}
+          onLongPress={decrementRight}
           disabled={isLeftWinner() || isRightWinner()}>
           <Text style={[styles.score, textRightStyle()]}>{countRight}</Text>
         </TouchableOpacity>
@@ -214,6 +339,16 @@ const styles = StyleSheet.create({
     fontSize: 36,
     // borderColor: 'red',
     // borderWidth: 1,
+  },
+  inputName: {
+    position: 'absolute',
+    top: 16,
+    fontSize: 36,
+    flex: 1,
+    color: '#FFFFFF90',
+    // borderColor: 'red',
+    // borderWidth: 1,
+    paddingVertical: 0,
   },
 });
 
